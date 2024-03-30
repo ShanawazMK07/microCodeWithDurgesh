e0a9ab2ce669218372313d08c9ae185d2b57c76c
shan_demo2 <shahanwaz.k1998@gmail.com>
2024-03-30 17:16:16 +0530

    This changes is done Sayed Mani BHai

---
+++ UserService/UserService/src/main/java/com/lcwd/user/service/shan/ManiBhai.java
@@ -0,0 +1,8 @@
+package com.lcwd.user.service.shan;
+
+public class ManiBhai {
+
+    public static void main(String[] args) {
+        System.out.println("Mani Bhai is not giving not money to Sayed Bhai but taking all money from Sayed Bhai");
+    }
+}
