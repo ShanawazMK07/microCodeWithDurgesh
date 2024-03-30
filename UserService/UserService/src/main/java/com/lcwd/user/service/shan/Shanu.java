e34dd69c01c63e28669b022c91dc5cd2dd32c925
shan_demo2 <shahanwaz.k1998@gmail.com>
2024-03-30 16:50:48 +0530

    added a class

---
+++ UserService/UserService/src/main/java/com/lcwd/user/service/shan/Shanu.java
@@ -0,0 +1,8 @@
+package com.lcwd.user.service.shan;
+
+public class Shanu {
+    public static void main(String[] args) {
+        System.out.println("Hello Naushad Sir You are Our Guru");
+    }
+
+}
